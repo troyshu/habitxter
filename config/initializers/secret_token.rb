98e189660929733af72a249f76f73fee071adb4d
Troy Shu <tmshu1@gmail.com>
2012-11-11 07:46:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Habitxter::Application.config.secret_token = 'b0dcdffb16bf8f903662f2b183395c8901355f118192049d30a5237aa2123cda9f37296116ffc7dc4af5e442434be68365f3e66d80a3e27b780eefc0b730e556'
