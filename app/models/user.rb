4f7e2ef66f07658177987541d95b158a1c4d3e09
Troy Shu <tmshu1@gmail.com>
2012-11-12 15:17:40 -0500

    create User model

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+  attr_accessible :crypted_password, :email, :password_salt, :perishable_token, :persistence_token, :username
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Habitxter::Application.initialize!
-
-config.gem "authlogic"
